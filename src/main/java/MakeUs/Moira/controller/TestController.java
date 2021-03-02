3b8b03a7b95af8fedac2baca0bb774d309dd167e
welshimeat <kimsw1726@naver.com>
2021-03-02 23:37:05 +0900

    feat: Logback을 통한 로깅 기능 추가

--- src/main/java/MakeUs/Moira/controller/TestController.java
+++ src/main/java/MakeUs/Moira/controller/TestController.java
@@ -1,12 +1,17 @@
 package MakeUs.Moira.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/test")
     public String getTest(){
+        logger.info("INFO");
         return "hello";
     }
 }
