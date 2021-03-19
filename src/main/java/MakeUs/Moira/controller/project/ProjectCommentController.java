531a29aef8459cc75e5536c34796f7739986ffd4
welshimeat <kimsw1726@naver.com>
2021-03-19 13:18:49 +0900

    fix: mariaDB 스키마 다시 생성

--- src/main/java/MakeUs/Moira/controller/project/ProjectCommentController.java
+++ src/main/java/MakeUs/Moira/controller/project/ProjectCommentController.java
@@ -38,7 +38,7 @@ public class ProjectCommentController {
         return responseService.mappingSingleResult(commentId, "프로젝트 댓글 추가 성공");
     }
 
-    @ApiOperation(
+    @ApiOperation( 
             value = "프로젝트(팀) 댓글 조회",
             notes = "프로젝트(팀)의 댓글을 조회합니다. 프로젝트에 생성된 모든 댓글을 최신순으로 제공합니다")
     @GetMapping("/{projectId}")
