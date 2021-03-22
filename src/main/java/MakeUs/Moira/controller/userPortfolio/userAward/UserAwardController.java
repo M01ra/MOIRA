5b756939e37f9a5ab136d86dc0f54a0ca6f423e7
asdfg5415 <asdfg5415@gmail.com>
2021-03-23 01:10:48 +0900

    fix: API 번호 수정

--- src/main/java/MakeUs/Moira/controller/userPortfolio/userAward/UserAwardController.java
+++ src/main/java/MakeUs/Moira/controller/userPortfolio/userAward/UserAwardController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Api(tags = {"마이페이지-프로필 수정-선택정보"})
+@Api(tags = {"9.마이페이지-프로필 수정-선택정보"})
 @RequiredArgsConstructor
 @RestController
 public class UserAwardController {
